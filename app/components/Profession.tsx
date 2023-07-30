5ee29bdbd13a159f102996df89d7c336cdb7b968
alaminsahed <alaminsahed97@gmail.com>
2023-07-31 01:47:17 +0600

    feat: add experience structure

--- app/components/Profession.tsx
+++ app/components/Profession.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
 import Bounce from 'react-reveal/Bounce';
@@ -5,6 +6,23 @@ import LightSpeed from 'react-reveal/LightSpeed';
 import Link from 'next/link';
 import { cn } from '@/libs/utils';
 import { totalExperience } from '@/utils/data/experience';
+
+// export const projectTagVariant = {
+// 	dark: "bg-gray-100 text-gray-800 p-10 font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500",
+// 	default:
+// 		"bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400",
+// 	red: "bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400",
+// 	green:
+// 		"bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400",
+// 	yellow:
+// 		"bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-400 border border-yellow-400",
+// 	purple:
+// 		"bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400",
+// 	indigo:
+// 		"bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400",
+// 	pink: "bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-pink-400 border border-pink-400",
+// };
+
 
 
 const Profession = () => {
@@ -18,12 +36,12 @@ const Profession = () => {
 					</Zoom>
 				</h2>
 			</div>
-			<div className="grid grid-cols-2 divide-x-4 p-5 divide-lime-300">
+			<div className="grid sm:grid-cols-2 sm:divide-x-4 p-5 sm:divide-lime-300">
 				{
 					totalExperience.map((experience, index) => (
 						<div className='p-3' key={index}>
 							<LightSpeed left delay={experience.animationDelay}>
-								<div className={cn("shadow-2xl rounded-lg p-6 dark:${experience.darkTextColor", experience.darkBgColor, experience.darkTextColor)}>
+								<div className="shadow-2xl rounded-lg p-6 dark:bg-gray-900">
 									<h3 className='text-xl sm:text-2xl font-bold text-rose-400'>{experience.title} ({experience.area})</h3>
 									<h4 className=' text-base sm:text-lg font-semibold'><Link href={experience.company_url}>{experience.company}</Link></h4>
 									<h5>{experience.duration}</h5>
@@ -35,9 +53,12 @@ const Profession = () => {
 								{
 									experience.projects.map((project, index) => (
 										<Bounce cascade duration={project.animationDuration} delay={project.animationDelay} key={index}>
-											<div className={cn("p-4 rounded-lg mt-3 flex justify-between", project.bgColor, project.darkBgColor)}>
-												<div className="text-base text-gray-500 dark:text-gray-400">
+											<div className="p-4 rounded-lg mt-3 flex flex-col bg-gray-300 dark:bg-gray-800">
+												<div className="text-base text-slate-600 dark:text-gray-400">
 													<Link href="https://apply.bigthinkcapital.com/" target='blank'>{project.name}</Link>
+													<div>
+														<small className='text-xs'>{project.description}</small>
+													</div>
 													<div className='pt-2'>
 														{
 															project.stacks.map((tag, index) => (
@@ -46,7 +67,7 @@ const Profession = () => {
 														}
 													</div>
 												</div>
-												<button className="text-sm p-1  hover:bg-white text-white hover:text-black sm:font-bold md:py-2 md:px-4 rounded">
+												<button className="text-sm p-1 bg-blue-900 hover:bg-white text-white hover:text-black sm:font-bold md:py-2 md:px-4 rounded mt-2 h-10 w-20">
 													Details
 												</button>
 											</div>
---
+++ utils/data/experience.ts
@@ -0,0 +1,182 @@
+export const projectTagVariant = {
+  dark: "bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500",
+  default:
+    "bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400",
+  red: "bg-red-100 text-red-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400",
+  green:
+    "bg-green-100 text-green-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400",
+  yellow:
+    "bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-400 border border-yellow-400",
+  purple:
+    "bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400",
+  indigo:
+    "bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400",
+  pink: "bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-1 md:px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-pink-400 border border-pink-400",
+};
+
+export const totalExperience = [
+  {
+    title: "Associate Software Engineer",
+    company: "SVAM International Inc.",
+    company_url: "https://www.svam.com/",
+    duration: "July 2022 - Present",
+    stacks: "React.js, Bootstrap, Tailwind CSS and Angular",
+    area: "Frontend",
+    companyDescription:
+      "SVAM International Inc. is a global Information Technology (IT) services provider that delivers value and competitive advantage to its customers by improving their access to critical information, automating their business processes, and helping their personnel collaborate effectively.",
+    animationDelay: 500,
+    projects: [
+      {
+        name: "Big Think Capital",
+        live_url: "https://apply.bigthinkcapital.com/",
+        description:
+          "Big Think Capital is a financial technology company that provides a platform for small businesses to access capital. The company offers a wide range of financial products and services to small businesses, including short-term loans, lines of credit, and equipment financing.",
+        responsibilities: [
+          "Developed and maintained the frontend of a project that managed over 3k merchants and lenders on a daily basis using React.js.",
+          "Creating new features and building reusable components using React.js, enhancing the functionality and interactivity of web applications.",
+          "Utilizing REST APIs and integrating various third-party APIs to incorporate external services and enrich the application's capabilities.",
+          "Demonstrating a deep understanding of business flows and implementing business logic on the frontend, ensuring a seamless and intuitive user experience.",
+          "Collaborating closely with backend developers to discuss API structures and data flow, fostering effective communication and alignment between frontend and backend systems.",
+          "Resolving bugs and issues, actively improving approximately 30-35% of the user experience through continuous enhancements and optimizations",
+          "Working collaboratively with both local and international team members to achieve project deadlines and goals, leveraging our collective strengths and expertise.",
+        ],
+        stacks: [
+          {
+            name: "React.js",
+            style: projectTagVariant.default,
+          },
+          {
+            name: "Bootstrap",
+            style: projectTagVariant.dark,
+          },
+          {
+            name: "TypeScript",
+            style: projectTagVariant.yellow,
+          },
+        ],
+        animationDuration: 2000,
+        animationDelay: 600,
+      },
+    ],
+  },
+  {
+    title: "Junior Web Developer",
+    company: "Unishopr.com",
+    company_url: "https://unishopr.com/",
+    duration: "Aug 2021 - June 2022",
+    stacks:
+      "React.js, Next.js, Bootstrap, Tailwind CSS, Node.js, Express.js, Hasura and MongoDB",
+    area: "Full Stack",
+    companyDescription:
+      "UniShopr is an overseas online shopping platform to get items from anywhere in the world. UniShopr is an overseas online shopping platform to get items from anywhere in the world.",
+    animationDelay: 500,
+    projects: [
+      {
+        name: "Unishopr.com",
+        live_url: "https://unishopr.com/",
+        description:
+          "UniShopr is an overseas online shopping platform to get items from anywhere in the world.",
+        responsibilities: [
+          "Developed and maintained the frontend and backend of an e-commerce website, serving over 500 orders daily.",
+          "Developed new features and reusable components using React.js, ensuring high-quality code and seamless user experiences.",
+          "Built APIs utilizing Hasura, GraphQL, and REST with Node.js and Express.js.",
+          "Conducted thorough bug fixing and troubleshooting, both on the frontend and backend, to improve overall application stability.",
+          "Played a key role in feature additions and conducted manual debugging and testing in an existing project.",
+          "Conducted technical research and development to enhance the user experience, resulting in a significant 20% growth in user engagement and satisfaction.",
+        ],
+        stacks: [
+          {
+            name: "React.js",
+            style: projectTagVariant.default,
+          },
+          {
+            name: "Next.js",
+            style: projectTagVariant.dark,
+          },
+          {
+            name: "TypeScript",
+            style: projectTagVariant.yellow,
+          },
+          {
+            name: "Node.js",
+            style: projectTagVariant.green,
+          },
+          {
+            name: "Hasura",
+            style: projectTagVariant.indigo,
+          },
+        ],
+        bgColor: "bg-gray-300",
+        darkBgColor: "dark:bg-gray-800",
+        animationDuration: 2000,
+        animationDelay: 600,
+      },
+      {
+        name: "Unishopr Pilot",
+        live_url: "",
+        description:
+          "UniShopr pilot is a internal project for UniShopr.com. It is a platform for the overseas delivery team to manage their flying time, flying and orders history.",
+        responsibilities: [
+          "Developed this project for 50+ internal users to manage their flying time and orders.",
+          "Developed new features and reusable components using React.js",
+          "Developed APIs using MongoDB and REST with Node.js and Express.js.",
+        ],
+        stacks: [
+          {
+            name: "React.js",
+            style: projectTagVariant.default,
+          },
+          {
+            name: "Node.js",
+            style: projectTagVariant.green,
+          },
+          {
+            name: "Express.js",
+            style: projectTagVariant.purple,
+          },
+          {
+            name: "MongoDB",
+            style: projectTagVariant.indigo,
+          },
+        ],
+        bgColor: "bg-gray-300",
+        darkBgColor: "dark:bg-gray-800",
+        animationDuration: 2000,
+        animationDelay: 600,
+      },
+      {
+        name: "Desh Door",
+        live_url: "",
+        description:
+          "Desh Door was a pilot project for UniShopr.com. Bangladeshi people who live in abroad can order products from Bangladesh through this platform.",
+        responsibilities: [
+          "Developed this project from scratch to understand the customer demands and market.",
+          "Developed new features and reusable components using React.js",
+          "Developed APIs using MongoDB and REST with Node.js and Express.js.",
+        ],
+        stacks: [
+          {
+            name: "React.js",
+            style: projectTagVariant.default,
+          },
+          {
+            name: "Node.js",
+            style: projectTagVariant.green,
+          },
+          {
+            name: "Express.js",
+            style: projectTagVariant.purple,
+          },
+          {
+            name: "MongoDB",
+            style: projectTagVariant.indigo,
+          },
+        ],
+        bgColor: "bg-gray-300",
+        darkBgColor: "dark:bg-gray-800",
+        animationDuration: 2000,
+        animationDelay: 600,
+      },
+    ],
+  },
+];
