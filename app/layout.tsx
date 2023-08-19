85272544c02bf087754a03d05c0b880eb15204a8
alaminsahed <alaminsahed97@gmail.com>
2023-08-20 05:32:42 +0600

    feat: add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import './globals.css'
-import 'animate.css';
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Providers from '../providers/providers';
+import "./globals.css";
+import "animate.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "../providers/providers";
+import { ToasterProvider } from "../providers/toasterProvider";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Md.Al-Amin Sahed',
-  description: 'A personal website for Md.Al-Amin Sahed as a React Developer',
-}
+  title: "Md.Al-Amin Sahed",
+  description: "A personal website for Md.Al-Amin Sahed as a React Developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className="font-serif tracking-widest" suppressHydrationWarning={true} >
+      <body
+        className="font-serif tracking-widest"
+        suppressHydrationWarning={true}
+      >
         <Providers>
+          <ToasterProvider />
           {children}
         </Providers>
       </body>
     </html>
-  )
+  );
 }
